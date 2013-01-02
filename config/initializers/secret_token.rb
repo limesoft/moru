373997fc8b16cc2dd4e8c172943088ec06ffe595
Zolzaya Erdenebaatar <zolzaya.erdenebaatar@gmail.com>
2013-01-02 14:16:18 +0800

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Moru::Application.config.secret_token = 'c13575a8e2aca41ed38ffbd314b4566078b6987d9c7b5c4f195f8a55a7fb3b2cc75998991e6e4c176702d12d352b4519e9a30c6e86017d0bb48e46d2cfb22b24'
