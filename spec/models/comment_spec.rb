ade35e4ab49c5e91e8c72af67d7ad66092eb0772
Zolzaya Erdenebaatar <zolzaya.erdenebaatar@gmail.com>
2013-01-04 16:16:42 +0800

    ашиглагдахгүй events controller-н тестийг устгаж, comment, rsvp модел дээр тест нэмэв

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -1,5 +1,11 @@
+# encoding: utf-8
+
 require 'spec_helper'
 
 describe Comment do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  it { should respond_to :user_id }
+  it { should respond_to :event_id }
+  it { should respond_to :content }
+
 end
--- spec/models/rsvp_spec.rb
+++ spec/models/rsvp_spec.rb
@@ -1,5 +1,11 @@
+# encoding: utf-8
+
 require 'spec_helper'
 
 describe Rsvp do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  it { should respond_to :going }
+  it { should respond_to :event_id }
+  it { should respond_to :user_id }
+
 end
