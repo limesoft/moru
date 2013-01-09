7e6741e0f028c4e7ff6abc7afa2742b3ad62b64f
Zolzaya Erdenebaatar <zolzaya.erdenebaatar@gmail.com>
2013-01-09 16:13:29 +0800

    rsvp_params-с going-н утгыг олон газар ашиглаж байсан тул нэгтгэв

--- app/controllers/rsvps_controller.rb
+++ app/controllers/rsvps_controller.rb
@@ -9,23 +9,23 @@ class RsvpsController < ApplicationController
   def create
     # 1. Эхлээд уг event-д rsvp өгсөн эсэхийг шалгана
     @rsvp = current_user.rsvp_for event
-    if @rsvp.present?
+    if @rsvp.present? && @rsvp.going != going
       # 2. Өгсөн байх юм бол rsvp-г шинэчлэнэ
-      @rsvp.going = rsvp_params[:going]
+      @rsvp.going = going
       @rsvp.save
     else
       # 3. Өгөөгүй байх юм бол шинээр үүсгэнэ
       # current_user-г RsvpContext-р өргөтгөж өгсөнөөр
       # уг хэрэглэгч RsvpContext-д зааж өгсөн чадваруудтай болно
       current_user.extend RsvpContext
-      @rsvp = current_user.create_rsvp(event, rsvp_params[:going])
+      @rsvp = current_user.create_rsvp(event, going)
     end
 
     # Уг event-г GoingContext-р өргөтгөж өгсөнөөр уг
     # event GoingContext-д зааж өгсөн чадваруудтай болно
     # Жишээ нь: count_rsvp, count_going гэх мэт
     event.extend GoingContext
-    @counter = event.count_rsvp(rsvp_params[:going])
+    @counter = event.count_rsvp(going)
     respond_with @rsvp
   end
 
@@ -34,7 +34,12 @@ class RsvpsController < ApplicationController
       @event ||= Event.find(rsvp_params[:event_id])
     end
 
+    def going
+      @rsvp_going ||= rsvp_params[:going]
+    end
+
     def rsvp_params
       params.require(:rsvp).permit(:event_id, :going)
     end
+
 end
