8ad9aa81c4d05b6983ec8381aff79560280f5a38
Zolzaya <zolzaya.erdenebaatar@gmail.com>
2013-01-28 21:28:44 +0800

    facebook-н wall-н тест

--- app/services/facebook_ogp_service.rb
+++ app/services/facebook_ogp_service.rb
@@ -12,6 +12,18 @@ class FacebookOgpService
   end
   handle_asynchronously :post_topic, run_at: Proc.new { 10.seconds.from_now }
 
+  def post_on_wall(user_id)
+    @user_id = user_id
+    ogp { |p| p.put_wall_post("Hello there!", {
+        "name" => "Link name",
+        "link" => "http://www.moru.mn",
+        "caption" => "I'm posted a new review",
+        "description" => "This is a longer description of the attachment"
+      })
+    }
+  end
+  handle_asynchronously :post_on_wall
+
   private
     def ogp
       @ogp ||= Koala::Facebook::API.new(user.try(:get_facebook_token))
