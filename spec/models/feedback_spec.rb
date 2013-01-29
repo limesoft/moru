99536783b701b04bcad97b57c5c2147bccf48e55
Zolzaya Erdenebaatar <zolzaya.erdenebaatar@gmail.com>
2013-01-29 16:25:18 +0800

    feedback model-н талбаруудын тестийг нэмэв

--- spec/models/feedback_spec.rb
+++ spec/models/feedback_spec.rb
@@ -1,5 +1,10 @@
+# encoding: utf-8
+
 require 'spec_helper'
 
 describe Feedback do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  it { should respond_to :content }
+  it { should respond_to :user_id }
+
 end
