20336d5a31806cb5c70a0283feb7c329ff3fbe41
Zolzaya Erdenebaatar <zolzaya.erdenebaatar@gmail.com>
2013-01-31 11:17:20 +0800

    topic controller-н create дээр тестийг нь бичив

--- spec/controllers/topics_controller_spec.rb
+++ spec/controllers/topics_controller_spec.rb
@@ -5,6 +5,8 @@ require 'spec_helper'
 describe TopicsController do
 
   let(:topic) { Fabricate(:topic) }
+  let(:user) { Fabricate(:user) }
+  let(:topic_params) { Fabricate.attributes_for(:topic) }
 
   context "GET index" do
     before(:each) { get :index }
@@ -16,6 +18,31 @@ describe TopicsController do
     before(:each) { get :show, id: topic.id }
     it { response.should render_template :show }
     it { assigns(:topic).should eq(topic) }
+  end
+
+  describe "POST create" do
+    before(:each) { login_in_as(user) }
+    context "#topic_type" do
+      it 'when it "yes"' do
+        topic_params[:type] = 'yes'
+        expect {
+          xhr :post, :create, topic: topic_params
+        }.to change(Topic, :count).by(1)
+      end
+
+      it 'when it "no"' do
+        topic_params[:type] = 'no'
+        expect {
+          xhr :post, :create, topic: topic_params
+        }.to change(Topic, :count).by(1)
+      end
+
+      it 'when it is not set' do
+        expect {
+          xhr :post, :create, topic: topic_params
+        }.to change(Topic, :count).by(0)
+      end
+    end
   end
 
 end
--- spec/support/controller_helper.rb
+++ spec/support/controller_helper.rb
@@ -1,7 +1,8 @@
 # encoding: utf-8
 
 module ControllerHelper
+
   def login_in_as(user)
-    controller current_user: user
+    controller.stub current_user: user
   end
 end
