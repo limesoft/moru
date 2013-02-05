9b8575d4cedee669361eb028ffb887536229a0de
Zolzaya Erdenebaatar <zolzaya.erdenebaatar@gmail.com>
2013-02-05 12:36:18 +0800

    going context-н тестийг сайжруулав

--- spec/context/going_spec.rb
+++ spec/context/going_spec.rb
@@ -5,19 +5,27 @@ require "spec_helper"
 describe GoingContext do
 
   let(:user) { Fabricate(:user) }
-  let(:event) { Fabricate(:event) }
-  subject { Fabricate(:rsvp, event: event, user: user).extend GoingContext }
+  subject { Fabricate(:event).extend GoingContext }
+  before(:all) do
+    4.times { subject.rsvps << Fabricate(:rsvp, going: RSVP_YES) }
+    5.times { subject.rsvps << Fabricate(:rsvp, going: RSVP_MAYBE) }
+    3.times { subject.rsvps << Fabricate(:rsvp, going: RSVP_NO) }
+    subject.save
+  end
 
   context "#count_going" do
     it { should respond_to :count_going }
+    its(:count_going) { should eq(4) }
   end
 
   context "#count_maybe" do
     it { should respond_to :count_maybe }
+    its(:count_maybe) { should eq(5) }
   end
 
   context "#count_not_going" do
     it { should respond_to :count_not_going }
+    its(:count_not_going) { should eq(3) }
   end
 
   context "#find_goings" do
--- spec/fabricators/fabricator.rb
+++ spec/fabricators/fabricator.rb
@@ -36,4 +36,5 @@ end
 
 Fabricator(:rsvp) do
   going { RSVPS.sample }
+  user
 end
